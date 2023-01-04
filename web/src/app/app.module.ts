94bd01978f48225845098543a2abb77a4cb74d37
TmaAgap2It <ladebrouille4588@gmail.com>
2023-01-04 20:00:37 +0100

    fix theming

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatMenuModule} from '@angular/material/menu'; 
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-import {MatMenuModule} from '@angular/material/menu'; 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     MatMenuModule,
-    BrowserAnimationsModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
