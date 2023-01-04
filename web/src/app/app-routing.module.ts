858462fa17b13dd61659f3def36919cd70edd22a
TmaAgap2It <ladebrouille4588@gmail.com>
2023-01-04 17:27:40 +0100

    create a player

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path:"",
+      component: MainPageComponent
+  },
+  {
+      path:"app-main-page",
+      component: MainPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './components/player/player.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
