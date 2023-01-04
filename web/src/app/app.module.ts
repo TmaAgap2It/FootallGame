00c5253c71d0d0baa9b622714f54a96ff100aa90
TmaAgap2It <ladebrouille4588@gmail.com>
2023-01-04 19:17:19 +0100

    topo formation angular

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import {MatMenuModule} from '@angular/material/menu'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/pages/main-page/main-page.component.ts
+++ web/src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+class IAnimal{
+  public name?:string;
+  public size:number = 0;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  pet:IAnimal={name:'Dog', size:10} as IAnimal;
+  petsList:string[] = ['Monkey'];
+  messageAutoDetruitAffiche:boolean = true;
 
+  ngOnInit(): void {
+    const autoDestructionPromise = new Promise<boolean>((resolve) => {
+      setTimeout(() => resolve(false), 5000);
+    });
+
+    autoDestructionPromise.then((afficher) => {
+      this.messageAutoDetruitAffiche = afficher;
+    });
+  }
+
+  changePet(){
+    this.pet = {...this.pet, name:'Cat'} as IAnimal;
+    this.petsList= [...this.petsList, 'Bird']
+  }
 }
